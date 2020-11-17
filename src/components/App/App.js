11aa89a180128e2c27ed525ef020cbe6f56d5321
Katherine Sinclair <sinc0115@algonquinlive.com>
2020-11-17 11:16:08 -0500

    completes section three

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,44 @@
-// import '../logo.svg'
-import React from 'react'
-import './App.css'
-import BusinessList from '../BusinessList/BusinessList'
-import SearchBar from '../SearchBar/SearchBar'
+import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList /> 
-    </div>
-  )
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOttoPizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
